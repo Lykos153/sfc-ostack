e6824147b636d00c3e4526f8d01f61071b227342
Silvio Ankermann <silvio@booq.org>
2018-06-13 13:35:08 +0200

    Store timestamps in array instad of appending it

--- sfc-ostack/demo/sfp/gap_sf_nc.py
+++ sfc-ostack/demo/sfp/gap_sf_nc.py
@@ -15,6 +15,7 @@ import socket
 import struct
 import sys
 import time
+import kodo
 
 ############
 #  Config  #
@@ -107,12 +108,7 @@ def forwards_forward(recv_sock, send_sock):
     while True:
         pack_len = recv_sock.recv_into(pack_arr, BUFFER_SIZE)
         # MARK: Maybe too slow here
-        recv_time_b = (b',' + str(time.time()).encode('ascii'))
-        ts_len = len(recv_time_b)
-
-        #####################
-        #  Mod UDP Payload  #
-        #####################
+        recv_time = time.time()
 
         # Header offset
         hd_offset = 0
@@ -125,19 +121,15 @@ def forwards_forward(recv_sock, send_sock):
             ver_ihl = struct.unpack('>B', pack_arr[hd_offset:hd_offset + 1])[0]
             ihl = 4 * int(hex(ver_ihl)[-1])
             # IP total length
-            old_ip_tlen = struct.unpack(
+            ip_tlen = struct.unpack(
                 '>H', pack_arr[hd_offset + 2:hd_offset + 4])[0]
             logger.debug(
                 'Recv a IP packet, header len: %d, total len: %d', ihl,
-                old_ip_tlen)
+                ip_tlen)
             proto = struct.unpack(
                 '>B', pack_arr[hd_offset + 9:hd_offset + 10])[0]
             # Check if is UDP packet
             if proto == 17:
-                logger.debug('Recv a UDP packet')
-                logger.debug(
-                    'Before appending time stamp, pack_len: %d', pack_len
-                )
                 hd_offset += ihl  # move to UDP header
                 udp_pl_offset = hd_offset + UDP_HDL
                 # Set checksum to zero
@@ -145,51 +137,17 @@ def forwards_forward(recv_sock, send_sock):
                 pack_arr[hd_offset + 6:hd_offset + 8] = struct.pack('>H', 0)
 
                 # UDP payload length
-                old_udp_pl_len = struct.unpack(
+                udp_pl_len = struct.unpack(
                     '>H', pack_arr[hd_offset + 4:hd_offset + 6]
                 )[0] - UDP_HDL
+                
+                udp_payload = pack_arr[udp_pl_offset:pack_len]
+                
+                timestamp[udp_payload] = recv_time
 
-                # Mod payload from b'a' to b'b'
-                pack_arr[udp_pl_offset:udp_pl_offset +
-                         PL_A_LEN] = b'b' * PL_A_LEN
-
-                # Append recv time stamp at end
-                pack_arr[udp_pl_offset + old_udp_pl_len:udp_pl_offset +
-                         old_udp_pl_len + ts_len] = recv_time_b
-
-                # Set UDP and IP total length with new payload
-                new_udp_tlen = struct.pack(
-                    '>H', (old_udp_pl_len + UDP_HDL + ts_len),
+                logger.debug('Recv data: %s, timestamp: %d', udp_payload, recv_time
                 )
-                pack_arr[hd_offset + 4:hd_offset + 6] = new_udp_tlen
-
-                hd_offset -= ihl
-                new_ip_tlen = struct.pack(
-                    '>H', (old_ip_tlen + ts_len)
-                )
-                pack_arr[hd_offset + 2:hd_offset + 4] = new_ip_tlen
-
-                # MARK: IP total length changed. MUST recalculate the IP header checksum
-                # TODO: For faster calc, this should be implemented in C
-                logger.debug(
-                    'Old IP header checksum: %s',
-                    binascii.hexlify(
-                        pack_arr[hd_offset + 10:hd_offset + 12]).decode()
-                )
-                # Set checksum field to zero
-                pack_arr[hd_offset + 10:hd_offset + 12] = struct.pack('>H', 0)
-                new_iph_cksum = calc_ih_cksum(
-                    pack_arr[hd_offset:hd_offset + ihl]
-                )
-                logger.debug('New IP header checksum: %s', hex(new_iph_cksum))
-                pack_arr[hd_offset + 10:hd_offset +
-                         # MARK: Convert to big-endian
-                         12] = struct.pack('<H', new_iph_cksum)
-                pack_len += ts_len
-                logger.debug(
-                    'After appending time stamp, pack_len: %d', pack_len
-                )
-
+                
                 pack_arr[0:MAC_LEN] = DST_MAC_B
                 send_sock.send(pack_arr[0:pack_len])
 
@@ -233,6 +191,8 @@ if __name__ == "__main__":
         ingress_iface = sys.argv[4]
         egress_iface = sys.argv[5]
 
+    timestamps = dict()
+    
     # Bind sockets and start forwards and backwards processes
     recv_sock, send_sock = bind_raw_sock_pair(ingress_iface, egress_iface)
     fw_proc = multiprocessing.Process(target=forwards_forward,
