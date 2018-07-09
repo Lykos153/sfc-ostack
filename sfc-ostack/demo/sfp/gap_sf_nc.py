50183bd027efffffaf09da560c68a24a9cd74a58
Silvio Ankermann <silvio@booq.org>
2018-07-09 15:17:48 +0200

    Add coding header functions

--- sfc-ostack/demo/sfp/gap_sf_nc.py
+++ sfc-ostack/demo/sfp/gap_sf_nc.py
@@ -262,6 +262,62 @@ def test_error_rate(receiver, packet_num, timeout=0.5, wait_time=0.05):
     sock.close()
     return 1-received_acks/packet_num
 
+def build_header(encoder, field_size, gen_seq, gen_size, symbol_len):
+    header = bytearray(4)
+    
+    assert encoder in range(8)
+    assert field_size in range(4)
+    assert gen_seq in range(4)
+    
+    first_byte = encoder<<4 | field_size<<2 | gen_seq
+    
+    header = struct.pack('!BBH', first_byte, gen_size, symbol_len)
+    
+    return header
+
+def parse_header(header):
+    first_byte, gen_size, symbol_len = struct.unpack('!BBH', header)
+    
+    encoder = first_byte>>4 & 0b1111
+    field_size = first_byte>>2 & 0b11
+    gen_seq = first_byte & 0b11
+    
+    return (encoder, field_size, gen_seq, gen_size, symbol_len)
+    
+def update_header(header, gen_seq=None):
+    if gen_seq:
+        assert gen_seq in range(4)
+        first_byte = struct.unpack('!B', header)
+        first_byte = first_byte&0b11111100 | gen_seq
+    
+    
+def convert_encoder(kodo_object):
+    if not kodo_object:
+        return (0,0)
+        
+    name = type(kodo_object).__name__
+        
+    available_encoders = {
+        'Fulcrum': 1,
+        'FullVector': 2,
+        'NoCode': 3,
+        'OnTheFly': 4,
+        'Perpetual': 5,
+        'SlidingWindow': 6,
+        'SparseFullVector': 7
+    }
+    available_sizes = {'4':1, '8':2, '16':3}
+    
+    if name[-1] in available_sizes:
+        field_size = available_sizes[name[-1]]
+        name = name[:-1]
+    else:
+        field_size = 0
+    
+    [encoder] = [available_encoders[i] for i in available_encoders if name.startswith(i)]
+
+    return (encoder, field_size)
+    
 if __name__ == "__main__":
 
     if len(sys.argv) >= 7:
